c36f5d9836549a7d89eecc26b51a1e943790d5e4
arnicfil <arnicfil@cvut.cz>
2025-11-05 18:02:23 -0500

    Add metrics middleware for counting the amount of requests

--- main.go
+++ main.go
@@ -6,7 +6,19 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetrixsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func readinessEndpoint(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
@@ -23,7 +35,24 @@ func readinessEndpoint(w http.ResponseWriter, r *http.Request) {
 	w.Write(bodyBytes)
 }
 
+func (cfg *apiConfig) hitsEndpoint(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	hitsValue := cfg.fileserverHits.Load()
+	w.Write(fmt.Appendf(nil, "Hits: %d", hitsValue))
+}
+
+func (cfg *apiConfig) resetEndpoint(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+
+	defer r.Body.Close()
+	w.WriteHeader(http.StatusOK)
+}
+
 func run() error {
+	var apiCfg apiConfig
 	filepathRoot, err := os.Getwd()
 	if err != nil {
 		return err
@@ -32,8 +61,10 @@ func run() error {
 	fileSystem := http.FileServer(http.Dir((filepathRoot)))
 
 	DefaultServeMux := http.NewServeMux()
-	DefaultServeMux.Handle("/app/", http.StripPrefix("/app/", fileSystem))
+	DefaultServeMux.Handle("/app/", apiCfg.middlewareMetrixsInc(http.StripPrefix("/app/", fileSystem)))
 	DefaultServeMux.HandleFunc("/healthz/", readinessEndpoint)
+	DefaultServeMux.HandleFunc("/metrics/", apiCfg.hitsEndpoint)
+	DefaultServeMux.HandleFunc("/reset/", apiCfg.resetEndpoint)
 
 	port := "8080"
 	s := &http.Server{
